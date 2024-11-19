72034a8cb1226c5ca92c871864fce9cd81fbd2f1
Puspol Mondal <120292061+puspol-mondal@users.noreply.github.com>
2024-11-19 21:36:51 +0600

    Add files via upload
    day-3

---
+++ Week-4/Day-3/A_Segment_with_Small_Sum.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+
+    ll s;
+    cin >> n >> s;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    int l = 0, r = 0, ans = 0;
+    ll sum = 0;
+    while (r < n)
+    {
+        sum += v[r];
+        if (sum <= s)
+        {
+            ans = max(ans, r - l + 1);
+        }
+        else
+        {
+            sum -= v[l];
+            l++;
+        }
+        r++;
+    }
+    cout << ans << "\n";
+
+    return 0;
+}
