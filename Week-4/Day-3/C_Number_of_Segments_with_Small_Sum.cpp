d08f81f8d67a5e79053b4f75dcc509a4b9864942
Puspol Mondal <120292061+puspol-mondal@users.noreply.github.com>
2024-11-20 21:45:29 +0600

    Add files via upload
    .........

---
+++ Week-4/Day-3/C_Number_of_Segments_with_Small_Sum.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    ll s;
+    cin >> s;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int l = 0, r = 0;
+    ll ans = 0;
+    ll sum = 0;
+
+    while (r < n)
+    {
+        sum += a[r];
+        if (sum <= s)
+        {
+            ans += r - l + 1;
+        }
+        else
+        {
+            while (sum > s && l <= r)
+            {
+                sum -= a[l];
+                l++;
+            }
+            if (sum <= s)
+            {
+                ans += r - l + 1;
+            }
+        }
+        r++;
+    }
+    cout << ans << "\n";
+
+    return 0;
+}
---
+++ Week-4/Day-3/Moneymaking.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int x, y;
+    cin >> x >> y;
+    ll ans = (x * 5000) + (y * 9800);
+    cout << ans << "\n";
+
+    return 0;
+}
