736fe32eea80cff16bdeb5fe93b5fccf80b7a76c
Puspol Mondal <120292061+puspol-mondal@users.noreply.github.com>
2025-01-02 22:13:12 +0600

    Add files via upload
    day-5

---
+++ week-9/day-1/B_Closest_to_the_Left.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < q; i++)
+    {
+        int key, l = 0, r = n - 1, mid, ans = -1;
+        cin >> key;
+        while (l <= r)
+        {
+            mid = (l + r) / 2;
+            if (key >= a[mid])
+            {
+                ans = mid;
+
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+        cout << (ans + 1) << "\n";
+    }
+
+    return 0;
+}
